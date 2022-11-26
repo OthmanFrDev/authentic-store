bd0595c2386c37740be25e9034c92490bbe02492
unknown <perso@OFROUKH1.emea.cegedim.grp>
2022-11-27 00:40:47 +0100

    init dev

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import menu from './menu-items.json'
+export default function Navbar() {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav)
+    }
+    return (
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
+            <h1 className='w-full text-3xl font-bold text-black'>Store.</h1>
+            <ul className='hidden md:flex'>
+            {menu.items.map(item=><li key={item} className='p-4 whitespace-nowrap'>{item}</li>)}
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] z-40 h-full border-r border-r-gray-900 bg-[#E7E7E7] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                <h1 className='w-full text-3xl font-bold text-black m-4'>Store.</h1>
+                <ul className='p-4 uppercase'>
+                    {menu.items.map(item=><li key={item} className='p-4 border-b border-gray-600 whitespace-nowrap'>{item}</li>)}
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,73 @@
+import React , {useState,useEffect,useRef} from 'react'
+import {AiOutlineVerticalRight,AiOutlineVerticalLeft} from 'react-icons/ai'
+
+export default function Slider() {
+
+    const sliderImages = ['/assets/gucci.jpg', '/assets/gucci1.webp', '/assets/gucci2.webp', '/assets/gucci3.webp']
+    let count = 0;
+    let slideInterval;
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const slideRef = useRef();
+
+    const removeAnimation = () => {
+        slideRef.current.classList.remove("fade-anim");
+    };
+
+    useEffect(() => {
+        slideRef.current.addEventListener("animationend", removeAnimation);
+        slideRef.current.addEventListener("mouseenter", pauseSlider);
+        slideRef.current.addEventListener("mouseleave", startSlider);
+
+        startSlider();
+        return () => {
+            pauseSlider();
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    const startSlider = () => {
+        slideInterval = setInterval(() => {
+            handleOnNextClick();
+        }, 3000);
+    };
+
+    const pauseSlider = () => {
+        clearInterval(slideInterval);
+    };
+
+    const handleOnNextClick = () => {
+        count = (count + 1) % sliderImages.length;
+        setCurrentIndex(count);
+        slideRef.current.classList.add("fade-anim");
+    };
+    const handleOnPrevClick = () => {
+        const productsLength = sliderImages.length;
+        count = (currentIndex + productsLength - 1) % productsLength;
+        setCurrentIndex(count);
+        slideRef.current.classList.add("fade-anim");
+    };
+
+    return (
+        <div ref={slideRef} className="w-full select-none relative">
+            <div className="aspect-w-16 aspect-h-9">
+                <img src={sliderImages[currentIndex]} alt="" />
+            </div>
+
+            <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center">
+                <button
+                    className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
+                    onClick={handleOnPrevClick}
+                >
+                    <AiOutlineVerticalRight size={30} />
+                </button>
+                <button
+                    className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
+                    onClick={handleOnNextClick}
+                >
+                    <AiOutlineVerticalLeft size={30} />
+                </button>
+            </div>
+        </div>
+    )
+}
