504ad51984d5649eb11998025f80ae6940665185
unknown <perso@OFROUKH1.emea.cegedim.grp>
2022-12-07 23:27:58 +0100

    adding template of product details

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Categories from '../../components/categories/Categories'
 import Slider from '../../components/slider/Slider'
+import ProductDetails from '../product-details/ProductDetails'
 
 function Home() {
     return (
         <>
             <Slider />
             <Categories />
+            <ProductDetails />
         </>
     )
 }
---
+++ src/pages/product-details/ProductDetails.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function ProductDetails() {
+    return (
+        <div className="hidden md:grid lg:grid max-w-[1340px]  grid-cols-2 gap-0">
+            <div>
+                <img className='w-full h-[400px] object-cover' src='https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60' />
+            </div>
+            <div>
+                <img className='w-full h-[400px] object-cover' src='https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60' />
+            </div>
+            <div>
+                <img className='w-full h-[400px] object-cover' src='https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60' />
+            </div>
+            <div>
+                <img className='w-full h-[400px] object-cover' src='https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60' />
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetails
